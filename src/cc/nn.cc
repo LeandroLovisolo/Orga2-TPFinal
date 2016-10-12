9194e7152ad21b3c2aace6f37174d22a0c9bd905
Leandro Lovisolo <leandro@leandro.me>
2016-10-12 17:33:13 -0400

    Support for stats generation

--- src/cc/nn.cc
+++ src/cc/nn.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 #include "matrix.h"
 #include "mnist_loader.h"
@@ -27,7 +28,8 @@ TrainingData<Vector> to_training_data(const LabelledMistData& mnist_data) {
 }
 
 template<class Matrix, class Vector=Matrix>
-void TrainNetwork(const string& matrix_impl_name) {
+void TrainNetwork(const string& matrix_impl_name, int epochs,
+                  const string& stats_file) {
   MnistLoader loader(kTrainImagesPath, kTrainLabelsPath,
                      kTestImagesPath, kTestLabelsPath);
 
@@ -38,7 +40,7 @@ void TrainNetwork(const string& matrix_impl_name) {
 
   cout << "Training with matrix implementation: " << matrix_impl_name << endl;
   Network<Matrix> nn(vector<int> { 784, 30, 10 });
-  nn.SGD(training_data, test_data, 100, 50, 0.1);
+  nn.SGD(training_data, test_data, stats_file, epochs, 50, 0.1);
   cout << "Training finished." << endl;
 }
 
@@ -51,16 +53,26 @@ int main(int argc, char **argv) {
           "naive, simd, eigen (default).")
     .set_default("eigen")
     .metavar("MATRIX_IMPL");
+  parser.add_option("-n" "--num-epochs").dest("epochs")
+    .help("Number of training epochs (default: 100)")
+    .set_default("100")
+    .metavar("EPOCHS");
+  parser.add_option("-s", "--stats").dest("stats_file")
+    .help("File to which write out stats.")
+    .set_default("")
+    .metavar("STATS_FILE");
 
   const optparse::Values options = parser.parse_args(argc, argv);
   string impl = options["matrix"];
+  int epochs = stoi(options["epochs"]);
+  string stats_file = options["stats_file"];
 
   if(impl == "naive") {
-    TrainNetwork<NaiveMatrix>(impl);
+    TrainNetwork<NaiveMatrix>(impl, epochs, stats_file);
   } else if(impl == "simd") {
-    TrainNetwork<SimdMatrix>(impl);
+    TrainNetwork<SimdMatrix>(impl, epochs, stats_file);
   } else if(impl == "eigen") {
-    TrainNetwork<EigenMatrix>(impl);
+    TrainNetwork<EigenMatrix>(impl, epochs, stats_file);
   } else {
     cerr << "Invalid MATRIX_IMPL. Run with --help to see valid options."
          << endl;
